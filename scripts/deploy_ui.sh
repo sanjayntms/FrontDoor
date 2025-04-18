99401172864f23fa07b746c903b4082dd79002aa
Sanjay NTMS <sanjayntms@yahoo.com>
2025-04-18 19:59:19 +0530

    1

---
+++ scripts/deploy_ui.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# (Script content from previous response)
+echo "This is a placeholder for deploy_ui.sh"
---
+++ scripts/upload_images.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# (Script content from previous response)
+echo "This is a placeholder for upload_images.sh"
